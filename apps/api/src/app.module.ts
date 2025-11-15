f7d2d0ebfa44f2e5b4b19fa938048444e4c7a13b
jose.cvi <jose.cvi@avangenio.com>
2025-11-15 17:22:43 -0500

    feat(api-app): register missing modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { OryClientModule } from './ory-client/ory-client.module';
+import { KratosModule } from './kratos/kratos.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    OryClientModule,
+    KratosModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

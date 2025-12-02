a747b90aa3edd5df986c4f1ee42bc5cae57c0e80
Carlos Raul <carlitos47614@gmail.com>
2025-12-02 03:07:03 -0500

    feat: implemented password recovery flow

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,15 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResultToExceptionInterceptor } from './common/interceptors/result-to-exception.interceptor';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new ResultToExceptionInterceptor());
-
+  app.use(cookieParser());
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:4455',
+    origin: process.env.CORS_ORIGIN || 'http://localhost:8362',
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie', 'Set-Cookie'],
+    exposedHeaders: ['Set-Cookie'], // Important for cookie debugging
   });
 
   const config = new DocumentBuilder()
